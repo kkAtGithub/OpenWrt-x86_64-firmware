d43dd637f8311d87d05971e9eee43d4248db5268
kk <zky.nais@gmail.com>
2021-11-16 14:25:52 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@
 git clone https://github.com/shadowsocks/openwrt-feeds.git package/feeds
 git clone https://github.com/shadowsocks/openwrt-shadowsocks.git package/shadowsocks-libev
 mkdir -p /workdir/openwrt/dl
-cp $GITHUB_WORKSPACE/pcre-8.45.tar.gz /workdir/openwrt/dl
+cp $GITHUB_WORKSPACE/pcre/* /workdir/openwrt/dl
