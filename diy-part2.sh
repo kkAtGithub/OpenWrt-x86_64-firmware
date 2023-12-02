57d5d8d29687fc49c867b80f531ffbd971276cee
kk <zky.nais@gmail.com>
2023-12-03 03:17:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,16 +14,16 @@
 sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_generate
 sed -i 's#root::0:0:99999:7:::#root:$1$ZxIx/sXb$/dUqWNOoYUIqaBMwR7AWb0:19028:0:99999:7:::#g' package/base-files/files/etc/shadow
 
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull origin master
-git branch --set-upstream-to=origin/master master
+#mkdir package/luci-app-openclash
+#cd package/luci-app-openclash
+#git init
+#git remote add -f origin https://github.com/vernesong/OpenClash.git
+#git config core.sparsecheckout true
+#echo "luci-app-openclash" >> .git/info/sparse-checkout
+#git pull origin master
+#git branch --set-upstream-to=origin/master master
 
 # 编译 po2lmo (如果有po2lmo可跳过)
-pushd luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd 
+#pushd luci-app-openclash/tools/po2lmo
+#make && sudo make install
+#popd 
